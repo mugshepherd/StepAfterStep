2cf9194b86f30ed709199a340748dd1e3cd33cdf
Toby <toby.schaeffer@gmail.com>
2015-04-12 20:50:38 -0400

    20150412-2050refactoring to bring rolls and threshold out of loop

--- data_incubator/dice.rb
+++ data_incubator/dice.rb
@@ -1,22 +1,21 @@
 #define mathematical methods
 module Enumerable
-
     def sum
-      self.inject(0){|accum, i| accum + i }
+        self.inject(0){|accum, i| accum + i }
     end
 
     def mean
-      self.sum/self.length.to_f
+        self.sum/self.length.to_f
     end
 
     def sample_variance
-      m = self.mean
-      sum = self.inject(0){|accum, i| accum +(i-m)**2 }
-      sum/(self.length - 1).to_f
+        m = self.mean
+        sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+        sum/(self.length - 1).to_f
     end
 
     def standard_deviation
-      return Math.sqrt(self.sample_variance)
+        return Math.sqrt(self.sample_variance)
     end
 end
 
@@ -52,34 +51,23 @@ def add_rolls
     temp_array = []
     series_details = []
     series_summed = []
-    series_post_threshold = []
     puts "how many separate roll series do you want?"
     rolls = gets.chomp.to_i
 
     while series_details.count < rolls
         temp_array = dice_roll(threshold)
         series_details << temp_array
-        # series_details << dice_roll.inject(:+)
-        # roll_series << dice_roll.dup.inject(:+) - $threshold
         series_summed << temp_array.reduce{|sum, x| (sum + x) } - 20
 
         # series_post_threshold << temp_array.reduce{|sum, x| sum + x}
         # series_post_threshold = series_post_threshold.dup.each{|x| }
-
-
-
     end
 
+    return series_details
 
-    puts "series details:  " + series_details.inspect
-    puts "series_summed details:  " + series_summed.inspect
-    puts "series_post_threshold details:  " + series_post_threshold.inspect
-
-    # puts "Series minus Threshold (#{$threshold}):  " + (series_details.reduce(:+).collect{|x| x - $threshold})
-    # puts roll_series.inspect
-
-    # puts "Average:  #{roll_series.mean}"
-    # puts "Standard Deviation of M = #{$threshold}:  #{roll_series.standard_deviation}"
+    # puts "series details:  " + series_details.inspect
+    # puts "series_summed details:  " + series_summed.inspect
+    # puts series_summed.reduce{|sum, x| (sum + x)}.mean
 
 
     #if
@@ -91,4 +79,9 @@ end
 
 
 #run code
-add_rolls
+
+
+puts add_rolls.reduce{|sum,x| (sum+x)}.mean
+puts add_rolls.reduce{|sum,x| (sum+x)}.sample_variance
+puts add_rolls.reduce{|sum,x| (sum+x)}.standard_deviation
+puts add_rolls.reduce{|sum,x| (sum+x)}.mean
